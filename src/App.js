3e79e5d758f76d36eec7bcb037224ebe804cd1f6
lchib <lchib@redhat.com>
2019-11-03 02:29:41 +0530

    Updated menu-items to navigate to their respective pages using withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+ 
+const Hats = () => (
+  <div>
+      <h1>HATS PAGE</h1>
+  </div>
+);
+
+const Jackets = () => (
+  <div>
+      <h1>JACKETS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-     <HomePage />
+      <BrowserRouter>
+     <Route exact path='/' component={HomePage} />
+     <Route exact path='/shop/hats' component={Hats} />
+     <Route path='/shop/jackets' component={Jackets} />
+     </BrowserRouter>
     </div>
   );
 }
